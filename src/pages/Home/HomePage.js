ff5468044fcab08ed944a5265b61456a06fb99dc
Monali Bedre <ajinkya.chanshetty@gmail.com>
2023-12-18 22:53:01 +0000

    Updated the home page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,9 +1,13 @@
+import { Faq } from "./components/Faq";
 import { Hero } from "./components/Hero";
+import { Testimonials } from "./components/Testimonials";
 
 export function HomePage() {
   return (
     <div>
       <Hero/>
+      <Testimonials/>
+      <Faq/>
     </div>
   )
 }
